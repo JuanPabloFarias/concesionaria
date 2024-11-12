9cc01a0029b32a177d8dfc9c833764dd6a944293
JuanPabloFarias <juanpablofarias16@gmail.com>
2024-11-12 19:34:40 -0300

    trabajo practico completado

--- concesionaria.js
+++ concesionaria.js
@@ -14,7 +14,42 @@ const concesionaria = {
     autoFinanciable:function (cuotas) {
         const enVenta = this.autosEnventa();
         return enVenta.filter(car => car.cuotas >= cuotas);
-    }
+    },
+//autosNuevos: retorna los vehiculos con menos de 100km.
+    autosNuevos: function(){
+        return this.vehiculos.filter(element => element.km < 100);
+    },
+
+    autosNuevos2() { 
+        return this.vehiculos.filter(car => car.km < 100)},
+// listaDeVentas: retorna la lista de los autos vendidos.
+    listaDeVentas: function(){
+        return this.vehiculos.filter(element => element.vendido === true);
+    },
+// totalDeVentas: me retorna la suma de los importes de los vehiculos vendidos.
+    totalDeVentas: function(){
+        let resultado = 0;
+        this.vehiculos.forEach(element => {
+            if(element.vendido === true){
+                resultado += element.precio;
+            }
+        })
+        return resultado
+    },
+
+    totalDeVentas2: function() { return this.listaDeVentas().reduce((total, vehiculo) => total + vehiculo.precio, 0); },
+// autosQuePuedeComprar: retorna la lista de los autos que sean menores o iguales al importe indicado por el cliente.
+    autosQuePuedeComprar: function(importe){
+        return this.vehiculos.filter(element => element.precio <= importe);
+    },
+
+    autosQuePuedeComprar2: function(importe) { return this.vehiculos.filter(vehiculo => vehiculo.precio <= importe && !vehiculo.vendido); },
 }
+
+//console.log(concesionaria.autosNuevos());
+//console.log(concesionaria.listaDeVentas());
+//console.log(concesionaria.totalDeVentas());
+//console.log(concesionaria.autosQuePuedeComprar(100000));
+console.log(concesionaria.autosQuePuedeComprar2(100000));
 
 module.exports = concesionaria;
